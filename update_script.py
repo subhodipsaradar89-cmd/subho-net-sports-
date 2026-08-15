1599829bbbb025b7b32e052e33543d8329bc8155
subhodipsaradar89-cmd <subhodipsaradar89@gmail.com>
2026-08-15 22:18:00 +0530

    Update update_script.py

--- update_script.py
+++ update_script.py
@@ -2,11 +2,13 @@ import os
 import json
 import requests
 
-url = "https://sonujson-v3.pages.dev/Data/sports.json"
+url = "https://sonujson-devloper.vercel.app/Data/sports.json"
 response = requests.get(url)
 data = response.json()
 
 os.makedirs("Data", exist_ok=True)
 
-with open("Data/subhotv.json", "w", encoding="utf-8") as f:
+with open("Data/subho_sports.json", "w", encoding="utf-8") as f:
     json.dump(data, f, indent=2)
+
+print("✅ subho_sports.json updated successfully!")
